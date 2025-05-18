fddee808a9abdce859055c70c490725af4d63e9c
Paul <paulallanmain@gmail.com>
2025-05-18 23:44:03 +0300

    feat: implement Customer model with name property and validation

---
+++ customer.py
@@ -0,0 +1,18 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name 
+
+    @name.settler
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")  
+
+
+        
